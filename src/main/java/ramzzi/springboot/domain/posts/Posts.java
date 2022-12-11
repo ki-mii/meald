221a0fa8db4ab77dd67bfd971b47f2d2d921bcdd
ki-mii <kjki93@naver.com>
2022-12-12 00:48:01 +0900

    jpa 시작

---
+++ src/main/java/ramzzi/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package ramzzi.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter //클래스 내 모든 필드의 getter 메소드 자동생성
+@NoArgsConstructor // 기본 생성자 자동 추가
+@Entity //테이블과 링크될 클래스
+public class Posts {
+
+    @Id // 해당 테이블의 PK 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK 생성규칙을 나타냄 같이 써주기
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
